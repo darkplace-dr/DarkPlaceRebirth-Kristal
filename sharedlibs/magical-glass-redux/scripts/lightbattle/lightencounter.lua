8bbca6bc7376ad78dc33c39fd9e1432d90f1f537
FireRainV <youaredied1234@gmail.com>
2025-02-05 16:55:56 +0200

    removed DT ost

--- sharedlibs/magical-glass-redux/scripts/lightbattle/lightencounter.lua
+++ sharedlibs/magical-glass-redux/scripts/lightbattle/lightencounter.lua
@@ -22,7 +22,7 @@ function LightEncounter:init()
     self.background_color = Game:isLight() and {34/255, 177/255, 76/255, 1} or {175/255, 35/255, 175/255, 1}
 
     -- The music used for this encounter
-    self.music = Game:isLight() and "battle_ut" or "battle_dt"
+    self.music = "battle_ut"
 
     -- Whether characters have the X-Action option in their spell menu
     self.default_xactions = Game:getConfig("partyActions")
