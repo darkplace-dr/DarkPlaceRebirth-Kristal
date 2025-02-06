82e524f567e542ca5d9ab7f69129347bf47db2fa
hyperboid <23138363+polypoyo@users.noreply.github.com>
2025-02-05 20:24:39 -0500

    Don't load MGR if we don't need it In theory, this would break a DLC that doesn't declare it's dependency of MGR for some reason. Pretty sure nobody's doing that though, so it's fine.

--- sharedlibs/dp/lib.lua
+++ sharedlibs/dp/lib.lua
@@ -10,4 +10,16 @@ function lib:onPause()
     end
 end
 
+function lib:save(data)
+    if not MagicalGlassLib then
+        data.magical_glass = self.mg_data_preserve
+    end
+end
+
+function lib:load(data)
+    if not MagicalGlassLib then
+        self.mg_data_preserve = data.magical_glass
+    end
+end
+
 return lib
