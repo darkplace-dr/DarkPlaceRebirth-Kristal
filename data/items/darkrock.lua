2293a43d85071087f3a75a5e6e4e25131bb40853
AcousticJamm <jasmicheals@gmail.com>
2025-04-22 18:01:38 -0400

    more darkrock stuff

--- data/items/darkrock.lua
+++ data/items/darkrock.lua
@@ -1,59 +1,37 @@
-local item, super = Class(Item, "darkrock")
+---@class Item.testdog : Item
+local item,super = Class(Item)
 
 function item:init()
     super.init(self)
-
-    -- Display name
     self.name = "Dark Rock"
-    -- Name displayed when used in battle (optional)
-    self.use_name = nil
-
-    -- Item type (item, key, weapon, armor)
-    self.type = "item"
-    -- Item icon (for equipment)
-    self.icon = nil
-
-    -- Battle description
+    self.saved_item = nil
+    self.usable_in = "none"
+	
+	-- Battle description
     self.effect = "Gone\nfor now"
     -- Shop description
     self.shop = "Musical food\nwith a\ncrunch\nHeals 80HP"
     -- Menu description
     self.description = "A dark stone that lost its original form.\nLooking closely, you see... more rock."
-
-    -- Default shop price (sell price is halved)
-    self.price = nil
-    -- Whether the item can be sold
-    self.can_sell = false
-
-    -- Consumable target mode (ally, party, enemy, enemies, or none)
-    self.target = "none"
-    -- Where this item can be used (world, battle, all, or none)
-    self.usable_in = "none"
-    -- Item this item will get turned into when consumed
-    self.result_item = nil
-    -- Will this item be instantly consumed in battles?
-    self.instant = false
-
-    -- Equip bonuses (for weapons and armor)
-    self.bonuses = {}
-    -- Bonus name and icon (displayed in equip menu)
-    self.bonus_name = nil
-    self.bonus_icon = nil
-
-    -- Equippable characters (default true for armors, false for weapons)
-    self.can_equip = {}
-
-    -- Character reactions (key = party member id)
-    self.reactions = {}
-
-    self.sounds = {}
+	
+	self.can_sell = false
+	
+	self.description = "A dark stone that lost its original form.\nLooking closely, you see... more rock."
 end
 
 function item:getDescription()
-	if self:getFlag("item_id") then
-		return "A dark stone that lost its original form.\nLooking closely, you see the label \"" .. self:getFlag("item_id") .. "\"."
+	if self.saved_item then
+		return "A dark stone that lost its original form.\nLooking closely, you see the label \"" .. self.saved_item .. "\"."
 	end
 	return self.description
+end
+
+function item:onLoad(data)
+    self.saved_item = data.id
+end
+
+function item:onSave(data)
+    data.id = self.saved_item
 end
 
 return item
