8d2064a58a1688fa25e1ae01012e700592a07221
JustAnotherRandomGithubUser <125609186+JustAnotherRandomGithubUser@users.noreply.github.com>
2025-04-24 12:46:23 -0400

    "movingarena" wave now supports Blue Mode.

--- mods/dpr_main/scripts/battle/waves/movingarena.lua
+++ mods/dpr_main/scripts/battle/waves/movingarena.lua
@@ -11,11 +11,21 @@ function MovingArena:onStart()
     -- Get the arena object
     local arena = Game.battle.arena
 
-    -- Spawn spikes on top of arena
-    self:spawnBulletTo(Game.battle.arena, "arenahazard", arena.width/2, 0, math.rad(0))
+	if Game.save_name == "BLUE" then
+        -- Spawn spikes on left of arena (rotated -90 degrees)
+        self:spawnBulletTo(Game.battle.arena, "arenahazard", 0, arena.height/2, math.rad(-90))
+    else
+        -- Spawn spikes on top of arena
+        self:spawnBulletTo(Game.battle.arena, "arenahazard", arena.width/2, 0, math.rad(0))
+    end
 
-    -- Spawn spikes on bottom of arena (rotated 180 degrees)
-    self:spawnBulletTo(Game.battle.arena, "arenahazard", arena.width/2, arena.height, math.rad(180))
+	if Game.save_name == "BLUE" then
+        -- Spawn spikes on right of arena (rotated 90 degrees)
+        self:spawnBulletTo(Game.battle.arena, "arenahazard", arena.width, arena.height/2, math.rad(90))
+    else
+        -- Spawn spikes on bottom of arena (rotated 180 degrees)
+        self:spawnBulletTo(Game.battle.arena, "arenahazard", arena.width/2, arena.height, math.rad(180))
+    end
 
     -- Store starting arena position
     self.arena_start_x = arena.x
@@ -26,11 +36,16 @@ function MovingArena:update()
     -- Increment timer for arena movement
     self.siner = self.siner + DT
 
-    -- Calculate the arena Y offset
+    -- Calculate the arena offset
     local offset = math.sin(self.siner * 1.5) * 60
 
-    -- Move the arena
-    Game.battle.arena:setPosition(self.arena_start_x, self.arena_start_y + offset)
+	if Game.save_name == "BLUE" then
+        -- Move the arena left and right
+        Game.battle.arena:setPosition(self.arena_start_x + offset, self.arena_start_y)
+    else
+        -- Move the arena up and down
+        Game.battle.arena:setPosition(self.arena_start_x, self.arena_start_y + offset)
+    end
 
     super.update(self)
 end
