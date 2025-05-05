3bcb8eb53ac59c2fbd5cd15982a211b1c024dba8
JustAnotherRandomGithubUser <125609186+JustAnotherRandomGithubUser@users.noreply.github.com>
2025-05-05 16:27:58 -0400

    Ceroba Master NPC.

--- mods/dpr_main/scripts/world/cutscenes/tutorialmaster.lua
+++ mods/dpr_main/scripts/world/cutscenes/tutorialmaster.lua
@@ -93,5 +93,26 @@ local tutorialmaster = {
         end
         master:setAnimation({ "idle" })
     end,
+    ceroba = function(cutscene, master)
+        cutscene:setTextboxTop(true)
+        master:setAnimation({ "bop" })
+        cutscene:text("* I'm Ceroba Master.\n[wait:5]* Ask me about CEROBA's.")
+        local choices = { "1", "2", "3" }
+        table.insert(choices, "Bye")
+        local c = cutscene:choicer(choices)
+        if c == 1 then
+            master:setAnimation({ "shocked" })
+            cutscene:text("* BING BING BING ONE.")
+        elseif c == 2 then
+            master:setAnimation({ "shocked" })
+            cutscene:text("* BING BING BING TWO.")
+        elseif c == 3 then
+            master:setAnimation({ "shocked" })
+            cutscene:text("* BING BING BING THREE.")
+        elseif c == 4 then
+            cutscene:text("* Later,[wait:5] kid.")
+        end
+        master:setAnimation({ "idle" })
+    end,
 }
 return tutorialmaster
