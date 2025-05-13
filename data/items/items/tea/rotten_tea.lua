1dd6e54254e86c2e21ac1107382ffc5e9ef681e8
AcousticJamm <jasmicheals@gmail.com>
2025-05-13 16:04:20 -0400

    Initial implementation of Rotten Tea

---
+++ data/items/items/tea/rotten_tea.lua
@@ -0,0 +1,53 @@
+local item, super = Class(HealItem, "rotten_tea")
+
+function item:init()
+    super.init(self)
+
+    -- Display name
+    self.name = "Rotten Tea"
+    -- Name displayed when used in battle (optional)
+    self.use_name = nil
+
+    -- Item type (item, key, weapon, armor)
+    self.type = "item"
+    -- Item icon (for equipment)
+    self.icon = nil
+
+    -- Battle description
+    self.effect = "Healing\nvaries"
+    -- Shop description
+    self.shop = ""
+    -- Menu description
+    self.description = "A tea that has expired."
+
+    -- Amount healed (HealItem variable)
+    self.heal_amount = 10
+
+    -- Default shop price (sell price is halved)
+    self.price = 10
+    -- Whether the item can be sold
+    self.can_sell = true
+
+    -- Consumable target mode (ally, party, enemy, enemies, or none)
+    self.target = "ally"
+    -- Where this item can be used (world, battle, all, or none)
+    self.usable_in = "all"
+    -- Item this item will get turned into when consumed
+    self.result_item = nil
+    -- Will this item be instantly consumed in battles?
+    self.instant = false
+
+    -- Equip bonuses (for weapons and armor)
+    self.bonuses = {}
+    -- Bonus name and icon (displayed in equip menu)
+    self.bonus_name = nil
+    self.bonus_icon = nil
+
+    -- Equippable characters (default true for armors, false for weapons)
+    self.can_equip = {}
+
+    -- Character reactions (key = party member id)
+    self.reactions = {}
+end
+
+return item
