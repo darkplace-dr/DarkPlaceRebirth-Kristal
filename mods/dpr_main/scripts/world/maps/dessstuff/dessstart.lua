424d39ac665f6ffddbf4788e9d056ffa1c5c8f4b
DIAMONDDELTAHEDRON <183671108+DIAMONDDELTAHEDRON@users.noreply.github.com>
2025-05-19 12:32:13 -0700

    desshere maps now have the white border

--- mods/dpr_main/scripts/world/maps/dessstuff/dessstart.lua
+++ mods/dpr_main/scripts/world/maps/dessstuff/dessstart.lua
@@ -1,7 +1,7 @@
 return {
-  version = "1.11",
+  version = "1.10",
   luaversion = "5.1",
-  tiledversion = "1.11.0",
+  tiledversion = "1.10.2",
   class = "",
   orientation = "orthogonal",
   renderorder = "right-down",
@@ -12,7 +12,7 @@ return {
   nextlayerid = 7,
   nextobjectid = 56,
   properties = {
-    ["border"] = "omori/whitespace'",
+    ["border"] = "white",
     ["music"] = "gimmieyourwalletmiss",
     ["name"] = "Dess - THE BEGINNING"
   },
