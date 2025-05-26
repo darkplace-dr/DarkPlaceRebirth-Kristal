d8a393ba7b10ddb91fc3eb3471cb618b7a0d79ce
AcousticJamm <jasmicheals@gmail.com>
2025-05-26 08:30:08 -0400

    Update acj_quest.lua

--- data/quests/acj_quest.lua
+++ data/quests/acj_quest.lua
@@ -4,7 +4,7 @@ local AcJQuest, super = Class(Quest, "acj_quest")
 function AcJQuest:init()
     super.init(self)
     self.name = "AcousticFamily"
-    self.description = "AcousticJamm's daughter, Marcy, states that her father mentioned a secret area in Deoxtest. Where could it be...?"
+    self.description = "Jamm's daughter, Marcy, states that her father mentioned a secret area in Deoxtest. Where could it be...?"
     self.progress = 0
     self.progress_max = 0
 end
@@ -15,9 +15,9 @@ end
 
 function AcJQuest:getDescription()
 	if Game:getFlag("acj_quest_prog", 0) == 1 then
-		return "You found AcousticJamm in the forest. However, something seems off about him. Not only that, but the forest seems different than when you entered. Try to find your way out!"
+		return "You found Jamm in the forest. However, something seems off about him. Not only that, but the forest seems different than when you entered. Try to find your way out!"
 	elseif Game:getFlag("acj_quest_prog", 0) == 2 then
-		return "You found AcousticJamm (for real this time) during your battle against Enzio. Now, AcousticJamm is on your side! However, with Enzio, this may only be the start..."
+		return "You found Jamm (for real this time) during your battle against Enzio. Jamm asked you to arrive to his apartment so he could properly thank you."
 	end
 	return self.description
 end
