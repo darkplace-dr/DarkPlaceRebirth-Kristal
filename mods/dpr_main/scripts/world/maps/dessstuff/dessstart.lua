e91e0d3877cdea58f49123171b8aadd92bbd626b
SadDiamondMan <139525343+SadDiamondMan@users.noreply.github.com>
2025-05-29 22:44:38 -0400

    Fixed Dess Respawning
    Dess no longer re-appears in desshere after desshere is complete

--- mods/dpr_main/scripts/world/maps/dessstuff/dessstart.lua
+++ mods/dpr_main/scripts/world/maps/dessstuff/dessstart.lua
@@ -1,7 +1,7 @@
 return {
-  version = "1.10",
+  version = "1.11",
   luaversion = "5.1",
-  tiledversion = "1.10.2",
+  tiledversion = "1.11.0",
   class = "",
   orientation = "orthogonal",
   renderorder = "right-down",
@@ -331,7 +331,7 @@ return {
           visible = true,
           properties = {
             ["actor"] = "dess",
-            ["cond"] = "not Game:isDessMode()",
+            ["cond"] = "not Game:getFlag(\"gotDess\", false) and not Game:isDessMode()",
             ["cutscene"] = "desslmao.dessbegin",
             ["flagcheck"] = "!gotDess"
           }
