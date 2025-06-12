517717009867eeda7d50c2085da0ef33118346f1
AcousticJamm <jasmicheals@gmail.com>
2025-06-12 11:38:08 -0400

    Marcy can influence Jamm

--- mods/dpr_main/scripts/world/events/teevie_quiz.lua
+++ mods/dpr_main/scripts/world/events/teevie_quiz.lua
@@ -747,6 +747,9 @@ function TeevieQuiz:update()
 									cutscene:wait(cutscene:walkTo(party3, self.button[6].x+20, party3.y, 6/30))
 									self.button[6]:press()
 									self.dess_wrong_answer = "B"
+								elseif Game.party[3].id == "jamm" and Game:getFlag("marcy_joined") and love.math.random() <= 0.2 then
+									cutscene:wait(cutscene:walkTo(party3, self.button[6].x+20, party3.y, 6/30))
+									self.button[6]:press()
 								else
 									cutscene:wait(cutscene:walkTo(party3, self.button[5].x+20, party3.y, 6/30))
 									self.button[5]:press()
@@ -758,6 +761,9 @@ function TeevieQuiz:update()
 									cutscene:wait(cutscene:walkTo(party3, self.button[5].x+20, party3.y, 6/30))
 									self.button[5]:press()
 									self.dess_wrong_answer = "A"
+								elseif Game.party[3].id == "jamm" and Game:getFlag("marcy_joined") and love.math.random() <= 0.2 then
+									cutscene:wait(cutscene:walkTo(party3, self.button[6].x+20, party3.y, 6/30))
+									self.button[5]:press()
 								else
 									cutscene:wait(cutscene:walkTo(party3, self.button[6].x+20, party3.y, 6/30))
 									self.button[6]:press()
