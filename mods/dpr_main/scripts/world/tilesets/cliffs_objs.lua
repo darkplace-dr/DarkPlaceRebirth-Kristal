85c840bd268bbb9c8852f0df837dda1650733086
Arlee-Puncher <177419396+Arlee-Puncher@users.noreply.github.com>
2025-06-16 15:32:33 -0300

    divine love

---
+++ mods/dpr_main/scripts/world/tilesets/cliffs_objs.lua
@@ -0,0 +1,47 @@
+return {
+  version = "1.10",
+  luaversion = "5.1",
+  tiledversion = "1.11.0",
+  name = "cliffs_objs",
+  class = "",
+  tilewidth = 180,
+  tileheight = 168,
+  spacing = 0,
+  margin = 0,
+  columns = 0,
+  objectalignment = "unspecified",
+  tilerendersize = "tile",
+  fillmode = "stretch",
+  tileoffset = {
+    x = 0,
+    y = 0
+  },
+  grid = {
+    orientation = "orthogonal",
+    width = 1,
+    height = 1
+  },
+  properties = {},
+  wangsets = {},
+  tilecount = 3,
+  tiles = {
+    {
+      id = 0,
+      image = "../../../assets/sprites/tilesets/objs/lamp_cliffs.png",
+      width = 70,
+      height = 136
+    },
+    {
+      id = 1,
+      image = "../../../assets/sprites/tilesets/objs/tree_cliffs.png",
+      width = 180,
+      height = 168
+    },
+    {
+      id = 2,
+      image = "../../../assets/sprites/tilesets/objs/dump.png",
+      width = 120,
+      height = 80
+    }
+  }
+}
