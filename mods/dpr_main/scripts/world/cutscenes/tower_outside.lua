4e1cfc1739da4a8d1dfa2074f57913fb3d1f6930
BrendaK7200 <87945117+BrendaK7200@users.noreply.github.com>
2025-06-26 14:01:48 -0400

    Re-added the Lazul quest
    Because Arlee made it not work for some reason

--- mods/dpr_main/scripts/world/cutscenes/tower_outside.lua
+++ mods/dpr_main/scripts/world/cutscenes/tower_outside.lua
@@ -1,7 +1,34 @@
 return {
+    lazul = function(cutscene)
+        local quest = Game:getFlag("package_quest")
+        if quest and quest == 1 then
+            cutscene:showNametag("Lazul", {color = {0, 0, 1, 1}})
+            cutscene:text("[color:blue]* In case you forgot the bin code:[wait:5] The code is [color:yellow]00000000[color:blue].")
+            cutscene:hideNametag()
+        elseif quest and quest > 1 then
+            cutscene:showNametag("Lazul", {color = {0, 0, 1, 1}})
+            cutscene:text("[color:blue]* We have nothing to discuss.")
+            cutscene:hideNametag()
+        else
+            cutscene:showNametag("Lazul", {color = {0, 0, 1, 1}})
+            cutscene:text("[color:blue]* Hey![wait:5] Do you happen to be a delivery man?[wait:5] No?[wait:5] Good.")
+            Game.inventory:addItem("diamond_package")
+            Assets.playSound("item")
+            cutscene:hideNametag()
+            cutscene:text("* You were given the [color:yellow]Diamond Package[color:reset].")
+            cutscene:text("* The [color:yellow]Diamond Package[color:reset] was added to your [color:yellow]KEY ITEMs[color:reset].")
+            cutscene:showNametag("Lazul", {color = {0, 0, 1, 1}})
+            cutscene:text("[color:blue]* Take that to the man who lives in the Warp Hub.[wait:5] Use the Warp Bin.")
+            cutscene:text("[color:blue]* The code is [color:yellow]00000000[color:blue].")
+            cutscene:hideNametag()
+            Game:getQuest("a_special_delivery"):unlock()
+            Game:setFlag("package_quest", 1)
+        end
+    end,
+    
 	start = function(cutscene, event)
         local hero = cutscene:getCharacter("hero")
-	Game.world.music:pause()
+	    Game.world.music:pause()
         hero:setFacing("down")
         hero:setSprite("fell")
         cutscene:wait(1)
@@ -24,7 +51,7 @@ return {
         Game:getQuest("wherethefuck"):unlock()
 	end,
 
-        trans = function(cutscene, event)
+    trans = function(cutscene, event)
         if love.math.random(1, 100) <= 5 and Game:getFlag("egg_h", false) then
             cutscene:mapTransition("tower/hell/hell_egg", "spawn")
             -- default wait func waits for the fade animation to end. movement should be allowed slightly before that
