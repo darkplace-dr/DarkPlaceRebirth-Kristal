f6e3bf65c9ba6842a891a7deb985a0eec0ce329b
Simbel0 <simbel.user@gmail.com>
2025-07-01 22:49:40 +0200

    Susie apartment dark world teaser

--- mods/dpr_light/scripts/world/maps/light/hometown/town_mid/data.lua
+++ mods/dpr_light/scripts/world/maps/light/hometown/town_mid/data.lua
@@ -1,7 +1,7 @@
 return {
   version = "1.10",
   luaversion = "5.1",
-  tiledversion = "1.10.2",
+  tiledversion = "1.10.0",
   class = "",
   orientation = "orthogonal",
   renderorder = "right-down",
@@ -856,8 +856,7 @@ return {
           rotation = 0,
           visible = true,
           properties = {
-            ["text1"] = "* (It's locked...)",
-            ["text2"] = "* (...[wait:5] the town it's pretty empty today.)"
+            ["cutscene"] = "hometown.susie_house"
           }
         },
         {
