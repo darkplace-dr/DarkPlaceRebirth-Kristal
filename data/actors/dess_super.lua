05876c9d99c81169a1fa8155e7f2d471284d3b02
Nelle <157313460+NelleMonelle@users.noreply.github.com>
2025-08-25 01:39:59 +0300

    Fixing battle offsets for Dess (and Super Dess)

--- data/actors/dess_super.lua
+++ data/actors/dess_super.lua
@@ -43,7 +43,7 @@ function actor:init()
         ["battle/spare"]         = {"battle/spell", 1/10, false, next="battle/idle"},
 
         ["battle/attack_ready"]  = {"battle/attackready", 0.2, true},
-        ["battle/act_ready"]     = {"battle/actready", 0.2, true, },
+        ["battle/act_ready"]     = {"battle/actready", 0.2, true},
         ["battle/spell_ready"]   = {"battle/spellready", 0.2, true},
         ["battle/item_ready"]    = {"battle/itemready", 0.2, true},
         ["battle/defend_ready"]  = {"battle/defend", 1/15, false},
@@ -52,11 +52,11 @@ function actor:init()
 
         ["battle/hurt"]          = {"battle/hurt", 1/15, false, temp=true, duration=0.5},
         ["battle/defeat"]        = {"battle/defeat", 1/15, false},
-		
+
 		["battle/transition"]    = {"battle/transition", 1/15, false},
         ["battle/intro"]   		 = {"battle/intro", 1/15, false},
         ["battle/victory"]       = {"battle/victory", 1/10, false},
-		
+
 		["battle/snap"]          = {"battle/snap", 2/30, false, next="battle/idle"},
 		["battle/spellsuper"]    = {"battle/spellsuper", 2/30, false, next="battle/spellsuperend"},
 		["battle/spellsuperend"] = {"battle/spellsuperend", 2/30, true},
@@ -74,39 +74,41 @@ function actor:init()
         ["walk/up"] = {-4, 0},
 
         -- Battle offsets
-        ["battle/idle"] = {-3, 0},
+        ["battle/idle"] = {-20, 0},
 
-        ["battle/attack"] = {-4, 0},
-        ["battle/attack_ready"] = {-4, 0},
-        ["battle/act"] = {0, 0},
-        ["battle/actend"] = {0, 0},
-        ["battle/act_ready"] = {0, 0},
-        ["battle/spell"] = {-3, 0},
-        ["battle/spell_ready"] = {0, 0},
-        ["battle/item"] = {-2, 0},
-        ["battle/item_ready"] = {-2, 0},
-        ["battle/defend"] = {-9, 0},
+        ["battle/attack"] = {-20, 0},
+        ["battle/attackready"] = {-20, 0},
+        ["battle/attackready_shake"] = {-20, 0},
+        ["battle/act"] = {-20, 0},
+        ["battle/actend"] = {-20, 0},
+        ["battle/actready"] = {-12, 0},
+        ["battle/spell"] = {-20, 0},
+        ["battle/spellready"] = {-20, 0},
+        ["battle/item"] = {-20, 0},
+        ["battle/itemready"] = {-20, 0},
+        ["battle/defend"] = {-20, 0},
 
-        ["battle/defeat"] = {0, 0},
-        ["battle/hurt"] = {-9, 0},
+        ["battle/defeat"] = {-12, 18},
+        ["battle/hurt"] = {-20, 0},
 
-        ["battle/transition"] = {-4, 0},
-        ["battle/intro"] = {-4, 0},
-        ["battle/victory"] = {-8, 0},
-		
-        ["battle/snap"] = {0, 0},
-        ["battle/spellsuper"] = {0, 0},
+        ["battle/transition"] = {-20, 0},
+        ["battle/intro"] = {-20, 0},
+        ["battle/victory"] = {-20, 0},
+
+        ["battle/snap"] = {-20, 0},
+        ["battle/spellsuper"] = {-19, 0},
+        ["battle/spellsuperend"] = {-18, 0},
     }
-	
+
 	self.shiny_id = "dess"
 end
 
 function actor:onSpriteInit(sprite)
     super.onSpriteInit(sprite)
-	
+
     sprite.flame = Assets.getFrames("effects/aura")
     sprite.flameframe = 0
-	
+
     sprite.flame1alpha = 0
     sprite.flame2alpha = 0
     sprite.flamecontimer = 0
@@ -118,12 +120,12 @@ function actor:preSpriteDraw(sprite)
 
     sprite.flamecontimer = sprite.flamecontimer + DTMULT
     sprite.specialcontimer = sprite.specialcontimer + DTMULT
-	
+
     sprite.flameframe = ((sprite.specialcontimer / 4) % 2)
-	
+
     sprite.flame1alpha = sprite.flamecontimer / 15
     sprite.flame2alpha = sprite.specialcontimer / 15
-	
+
     if sprite.flame2alpha >= 0.9 then
         sprite.flame2alpha = 0.9
     end
@@ -142,13 +144,13 @@ function actor:preSpriteDraw(sprite)
         Draw.draw(sprite.flame[1], 23/2, 46, 0, -1/2, (1.2 + math.abs((math.sin((sprite.specialcontimer / 4)) / 2)))/2, 99/2 + 10, 90)
     else
         Draw.draw(sprite.flame[1], 23/2 + 10, 46, 0, -1/2, (1.2 + math.abs((math.sin((sprite.specialcontimer / 4)) / 2)))/2, 99/2 + 10, 90)
-    end	
+    end
     love.graphics.setColor(0.5, 0.5, 0, (sprite.flame2alpha / 1.4))
     if not Game.battle or Game.battle and sprite.anim == "walk/right" then
         Draw.draw(sprite.flame[2], 23/2, 46, 0, -1/2, 1.4/2, 99/2 + 10, 90)
     else
         Draw.draw(sprite.flame[2], 23/2 + 10, 46, 0, -1/2, 1.4/2, 99/2 + 10, 90)
-    end	
+    end
 end
 
 return actor
