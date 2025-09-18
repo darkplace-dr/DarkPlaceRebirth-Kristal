076eb2efe261a78f1c16ff420ee3a16dc53388fe
JustAnotherRandomGithubUser <125609186+JustAnotherRandomGithubUser@users.noreply.github.com>
2025-09-17 21:12:52 -0400

    Finished Zapper's S-Action.

--- mods/dpr_main/scripts/battle/enemies/zapper.lua
+++ mods/dpr_main/scripts/battle/enemies/zapper.lua
@@ -51,6 +51,67 @@ function Zapper:init()
     self.killable = true
 
     self.volumeturnups = 0
+
+    self.used_s_action = false
+    self.changecolorcon = 0
+    self.changecolorcount = 0
+    self.changecolortimer = 0
+
+    self.colortarget = { 1, 1, 1 }
+    self.colortarget2 = { 1, 1, 1 }
+
+    self.fountain_snd = Assets.getSound("fountain_make")
+    self.soundtimer = 0
+    self.pitchtarget = 0
+    self.pitchtarget2 = 0
+    self.pitch = 0
+
+    self.closedcaptioncon = 0
+    self.closedcaptioncon2 = 0
+    self.closedcaptiontimer = 0
+end
+
+function Zapper:update()
+    super.update(self)
+
+    -- S-Action-related code
+    if self.used_s_action then
+        if not self.fountain_snd:isPlaying() then
+            self.fountain_snd:play()
+        end
+
+        if self.changecolorcon == 0 then
+            self.colortarget2 = {self:HSV(Utils.random(0, 255, 1)/255, 250/255, 255/255)}
+
+            if self.changecolorcount == 5 then
+                self.colortarget2 = { 1, 1, 1 }
+            end
+
+            self.pitchtarget2 = 1 + love.math.random(2)
+            self.changecolorcon = 1
+        end
+        if self.changecolorcon == 1 then
+            self.changecolortimer = self.changecolortimer + (1 * DTMULT)
+            self:setColor(Utils.mergeColor(self.colortarget, self.colortarget2, self.changecolortimer / 3))
+            self.pitch = Utils.lerp(self.pitchtarget, self.pitchtarget2, self.changecolortimer / 3)
+            self.fountain_snd:setPitch(self.pitch)
+
+            if self.changecolortimer == 3 then
+                self.colortarget = self.colortarget2
+                self.pitchtarget = self.pitchtarget2
+                self.changecolorcount = self.changecolorcount + (1 * DTMULT)
+                self.changecolorcon = 0
+                self.changecolortimer = 0
+            end
+        end
+
+        if self.changecolorcount == 6 then
+            self.fountain_snd:stop()
+            self:setColor(COLORS.white)
+            self.changecolorcount = 0
+            self.used_s_action = false
+        end
+    end
 end
 
 function Zapper:onAct(battler, name)
@@ -96,6 +157,13 @@ function Zapper:onAct(battler, name)
         if battler.chara.id == "susie" then
             self:addMercy(25)
             self.dialogue_override = "D-don't touch\nthat, you's!"
+
+            Assets.playSound("damage")
+            self:hurt(0, nil, nil, nil, false)
+            self:onHurt(0, nil)
+            self.hurt_timer = 1
+            self.used_s_action = true
+
             return "* Susie mashed random buttons!"
         elseif battler.chara.id == "ralsei" then
             self:addMercy(25)
@@ -107,6 +175,29 @@ function Zapper:onAct(battler, name)
     end
 
     return super.onAct(self, battler, name)
+end
+
+-- Function from the love2d wiki, converts HSV to RGB
+function Zapper:HSV(h, s, v)
+    if s <= 0 then return v,v,v end
+    h = h*6
+    local c = v*s
+    local x = (1-math.abs((h%2)-1))*c
+    local m,r,g,b = (v-c), 0, 0, 0
+    if h < 1 then
+        r, g, b = c, x, 0
+    elseif h < 2 then
+        r, g, b = x, c, 0
+    elseif h < 3 then
+        r, g, b = 0, c, x
+    elseif h < 4 then
+        r, g, b = 0, x, c
+    elseif h < 5 then
+        r, g, b = x, 0, c
+    else
+        r, g, b = c, 0, x
+    end
+    return r+m, g+m, b+m
 end
 
 return Zapper
