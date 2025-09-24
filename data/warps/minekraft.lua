2f9541fe94509477757b8588392d10eb76f265fc
GabrielcitoHub <82218456+GabrielcitoHub@users.noreply.github.com>
2025-09-24 03:12:19 -0300

    cave map warp file, could be used as example

---
+++ data/warps/minekraft.lua
@@ -0,0 +1,9 @@
+local warp = {}
+local self = warp
+
+self.id = "minekraft"
+self.variants = {"minecraft","deltacraft", "runecraft", "blockrune", "suscraft", "kricraft"}
+self.name = "Minekraft"
+self.warp = "cave"
+
+return self
