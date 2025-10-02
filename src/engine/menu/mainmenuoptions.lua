b801d558c310ac441bbac8fb4334d23a94348f9d
Simbel <61120464+Simbel0@users.noreply.github.com>
2025-10-02 04:56:51 +0200

    Fix BrokenMenuBox option being broken

--- src/engine/menu/mainmenuoptions.lua
+++ src/engine/menu/mainmenuoptions.lua
@@ -668,9 +668,7 @@ function MainMenuOptions:initializeOptions()
         love.window.setVSync(toggled and 1 or 0)
     end)
     self:registerConfigOption("graphics", "Frame Skip", "frameSkip")
-    self:registerConfigOption("graphics", "Broken Menu Boxes", "brokenMenuBoxes", function (toggled)
-        self.menu.mod_list:buildModList()
-    end)
+    self:registerConfigOption("graphics", "Broken Menu Boxes", "brokenMenuBoxes")
 
     ---------------------
     -- Engine Options
