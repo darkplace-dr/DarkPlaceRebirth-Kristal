1406409e6243a2b930af4dac05ade22e68e3d7e3
DIAMONDDELTAHEDRON <183671108+DIAMONDDELTAHEDRON@users.noreply.github.com>
2025-10-16 20:56:11 -0700

    Update tenna_ec.lua

--- mods/dpr_main/scripts/data/actors/tenna_ec.lua
+++ mods/dpr_main/scripts/data/actors/tenna_ec.lua
@@ -7,11 +7,11 @@ function actor:init()
     self.name = "DR:E!Tenna"
 
     -- Width and height for this actor, used to determine its center
-    self.width = 55
-    self.height = 76
+    self.width = 56
+    self.height = 80
 
     -- Hitbox for this actor in the overworld (optional, uses width and height by default)
-    self.hitbox = {0, 50, 55, 20}
+    self.hitbox = {0, 50, 56, 20}
 
     -- Color for this actor used in outline areas (optional, defaults to red)
     self.color = {1, 0, 0}
