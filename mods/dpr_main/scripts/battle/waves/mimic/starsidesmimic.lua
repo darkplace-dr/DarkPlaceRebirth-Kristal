5313428a01acf6ab73f111ae86cf74187e42e07c
JustAnotherRandomGithubUser <125609186+JustAnotherRandomGithubUser@users.noreply.github.com>
2025-10-18 21:25:36 -0400

    Fixed Mimic's UFO of Doom waves.

--- mods/dpr_main/scripts/battle/waves/mimic/starsidesmimic.lua
+++ mods/dpr_main/scripts/battle/waves/mimic/starsidesmimic.lua
@@ -21,7 +21,7 @@ function Basic:onStart()
         local y = Utils.random(Game.battle.arena.top, Game.battle.arena.bottom)
 
         -- Spawn smallbullet going left with speed 8 (see scripts/battle/bullets/smallbullet.lua)
-        local bullet = self:spawnBullet("starbullet", x, y, math.rad(direction), 2, 0.7)
+        local bullet = self:spawnBullet("ufos/starbullet", x, y, math.rad(direction), 2, 0.7)
 
         -- Dont remove the bullet offscreen, because we spawn it offscreen
         bullet.remove_offscreen = true
