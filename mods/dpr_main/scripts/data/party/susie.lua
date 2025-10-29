e98643bd802db821a8e625d69738c179ccab67e9
TheSkerch <theskerch@gmail.com>
2025-10-29 10:57:42 +0200

    Removed the evergroves(gonna move them to a DLC)

--- mods/dpr_main/scripts/data/party/susie.lua
+++
@@ -1,9 +0,0 @@
----@class PartyMember.susie : PartyMember
-local susie, super = Class("susie", true)
-
-function susie:init()
-    super.init(self)
-    self:setDepthsActor("susie_depths")
-end
-
-return susie
