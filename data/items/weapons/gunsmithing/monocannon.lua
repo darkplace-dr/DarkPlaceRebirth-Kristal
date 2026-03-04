9f172e54b7f5b28b6e84e0552bd5e9fcb169abf6
BrendaK7200 <87945117+BrendaK7200@users.noreply.github.com>
2026-03-03 21:09:43 -0500

    Grapeshot

---
+++ data/items/weapons/gunsmithing/monocannon.lua
@@ -0,0 +1,48 @@
+local item, super = Class(Item, "monocannon")
+
+function item:init()
+    super.init(self)
+
+    self.name = "MonoCannon"
+
+    self.type = "weapon"
+    self.icon = "ui/menu/icon/gun"
+
+    self.effect = ""
+    self.shop = ""
+    self.description = "A monochromatic gun made from Cliffside's stone.\nOnly increases defense."
+
+    self.price = 300
+    self.can_sell = true
+
+    self.target = "none"
+    self.usable_in = "all"
+    self.result_item = nil
+    self.instant = false
+
+    self.bonuses = {
+        attack = 0,
+        defense = 2,
+    }
+    self.bonus_name = nil
+    self.bonus_icon = nil
+
+    self.can_equip = {
+        brenda = true,
+        pauling = true,
+    }
+
+    self.reactions = {
+        susie = "",
+        ralsei = "",
+        noelle = "",
+		dess = "",
+        brenda = "",
+    }
+end
+
+function item:convertToLightEquip(inventory)
+    return "light/foam_dart_rifle" -- Placeholder
+end
+
+return item
