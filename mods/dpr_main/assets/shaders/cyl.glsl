26faf5d96dfcf938a7de8cb6e47b8378e64ba76b
Liu Wenyuan <15816141883@163.com>
2026-03-06 19:11:04 +0800

    dpr_main: Fix GLSL ES violations

--- mods/dpr_main/assets/shaders/cyl.glsl
+++ mods/dpr_main/assets/shaders/cyl.glsl
@@ -4,13 +4,13 @@ uniform float center_y;
 
 float outCirc(float time, float start, float change, float duration)
 {
-    time = time / duration - 1;
-    return change * sqrt(1 - pow(time, 2)) + start;
+    time = time / duration - 1.0;
+    return change * sqrt(1.0 - pow(time, 2.0)) + start;
 }
 
 float outSine(float time, float start, float change, float duration)
 {
-    return change * sin(time / duration * (3.14 / 2)) + start;
+    return change * sin(time / duration * (3.14 / 2.0)) + start;
 }
 
 vec4 effect( vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords )
@@ -20,21 +20,21 @@ vec4 effect( vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords 
     float center_dist_x = abs( 0.5 - texture_coords.x );
     float center_disp_y = center_y - texture_coords.y;
 
-    float squishy_percentage = min((1 + squish_softening) - (center_dist_x * 2), 1);
+    float squishy_percentage = min((1.0 + squish_softening) - (center_dist_x * 2.0), 1.0);
     float total_in_squish = 0.1 * squishiness;
 
-    float dist_y_squish_factor = abs( center_disp_y ) * 2;
+    float dist_y_squish_factor = abs( center_disp_y ) * 2.0;
 
-    float current_in_squish = outCirc( squishy_percentage, 0, total_in_squish * sign( center_disp_y ) * outSine( dist_y_squish_factor, 0, 1, 1 ), 1 );
+    float current_in_squish = outCirc( squishy_percentage, 0.0, total_in_squish * sign( center_disp_y ) * outSine( dist_y_squish_factor, 0.0, 1.0, 1.0 ), 1.0 );
 
     warped_coords.y = warped_coords.y - current_in_squish;
 
-    while (warped_coords.y > 1) {
-        warped_coords.y -= 1;
+    while (warped_coords.y > 1.0) {
+        warped_coords.y -= 1.0;
     }
 
-    while (warped_coords.y < 0) {
-        warped_coords.y += 1;
+    while (warped_coords.y < 0.0) {
+        warped_coords.y += 1.0;
     }
 
     vec4 texturecolor = Texel(texture, warped_coords);
