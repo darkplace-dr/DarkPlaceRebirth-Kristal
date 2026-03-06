662dda99f53eae87f3b1d88f69f6cd265a63eaf1
Hyperboid <hyperboid@proton.me>
2026-03-06 09:40:42 -0500

    Set bucket state to LOADED after finishing background loading

--- src/engine/loading/assets.lua
+++ src/engine/loading/assets.lua
@@ -299,6 +299,7 @@ function Assets.update()
                 end
             end
         end
+        bucket.state = AssetBucket.State.LOADED
     end
     Kristal.Loader.message = ""
     Kristal.Overlay.setLoading(false)
