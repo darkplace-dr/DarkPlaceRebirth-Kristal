7c1925382fa704d7b8a1b880c8b9d46fcc1f71db
Hyperboid <hyperboid@proton.me>
2026-03-08 19:07:18 -0400

    Improve FilePath constructor arguments

--- src/engine/loading/FilePath.lua
+++ src/engine/loading/FilePath.lua
@@ -1,13 +1,13 @@
 ---@class FilePath : Class
----@overload fun(base_path: string, full_path:string) : FilePath
+---@overload fun(base_path: string, relative_path: string) : FilePath
 local FilePath, super = Class(nil, "FilePath")
 
 ---@param base_path string
----@param full_path string
-function FilePath:init(base_path, full_path)
+---@param relative_path string
+function FilePath:init(base_path, relative_path)
     self.base_path = base_path
-    self.full_path = full_path
-    self.relative_path = full_path:sub(#(self.base_path)+2, -1)
+    self.relative_path = relative_path
+    self.full_path = self.base_path .. "/" .. self.relative_path
     
     local identifier, extension = unpack(StringUtils.splitFast(self.relative_path, "."))
     assert(identifier)
