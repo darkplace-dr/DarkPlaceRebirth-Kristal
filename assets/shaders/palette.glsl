892c2d36c1cdc2c5f494f37c2e957ff8a4d0d936
ddeltatemp <183671108+DIAMONDDELTAHEDRON@users.noreply.github.com>
2026-05-18 04:50:05 -0700

    Update palette.glsl

--- assets/shaders/palette.glsl
+++ assets/shaders/palette.glsl
@@ -16,7 +16,7 @@ vec4 find_alt_color(vec4 in_color)
 		left_color = Texel(palette_tex, test_pos);
         
 		dist = distance(left_color, in_color);
-+
+
 		if (dist < TOLERANCE) {
 			test_pos = vec2(0.0 + pixel_size.x * floor(palette_id + 1.0), i);
 			return mix(Texel(palette_tex, vec2(test_pos.x - pixel_size.x, test_pos.y)), Texel(palette_tex, test_pos), fract(palette_id));
