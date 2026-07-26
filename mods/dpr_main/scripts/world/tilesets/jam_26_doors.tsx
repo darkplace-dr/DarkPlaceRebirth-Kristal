45cddd88687c6b99fba38f9f54adeae877ff8bbf
Garb <gabrielcitobuenito@gmail.com>
2026-07-26 00:20:35 -0300

    added jam door sprites to their corresponding place trough a sprites collection tileset

---
+++ mods/dpr_main/scripts/world/tilesets/jam_26_doors.tsx
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.12.2" name="jam-26-doors" tilewidth="120" tileheight="160" tilecount="6" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="../../../assets/sprites/world/maps/floor2/darkjam_26/door_1.png" width="120" height="160"/>
+ </tile>
+ <tile id="1">
+  <image source="../../../assets/sprites/world/maps/floor2/darkjam_26/door_2.png" width="120" height="160"/>
+ </tile>
+ <tile id="2">
+  <image source="../../../assets/sprites/world/maps/floor2/darkjam_26/door_3.png" width="120" height="160"/>
+ </tile>
+ <tile id="3">
+  <image source="../../../assets/sprites/world/maps/floor2/darkjam_26/door_4.png" width="120" height="160"/>
+ </tile>
+ <tile id="4">
+  <image source="../../../assets/sprites/world/maps/floor2/darkjam_26/door_5.png" width="120" height="160"/>
+ </tile>
+ <tile id="5">
+  <image source="../../../assets/sprites/world/maps/floor2/darkjam_26/door_6.png" width="120" height="160"/>
+ </tile>
+</tileset>
