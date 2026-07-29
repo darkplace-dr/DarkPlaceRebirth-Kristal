7447f2e8a8469ee613ba823737d1b0fa8b1d2bb9
DragonZenore <163686699+DragonZenore@users.noreply.github.com>
2026-07-29 11:07:16 -0400

    Add files via upload

---
+++ mods/dpr_main/scripts/battle/waves/tailofhellstorm.lua
@@ -0,0 +1,28 @@
+local Tailofhellstorm, super = Class(Wave)
+
+function Tailofhellstorm:onStart()
+    -- Every 0.33 seconds...
+    self.timer:every(1/3, function()
+        -- Our X position is offscreen, to the right
+        local x = SCREEN_WIDTH + 20
+        -- Get a random Y position between the top and the bottom of the arena
+        local y = Utils.random(Game.battle.arena.top, Game.battle.arena.bottom)
+
+        -- Spawn smallbullet going left with speed 8 (see scripts/battle/bullets/smallbullet.lua)
+        local bullet = self:spawnBullet("tailofhell", x, y, math.rad(180), 8)
+		if Game.battle.encounter.volume_up then
+			bullet:setScale(0.25)
+			bullet.tp = 6.4
+		end
+        -- Dont remove the bullet offscreen, because we spawn it offscreen
+        bullet.remove_offscreen = false
+    end)
+end
+
+function Tailofhellstorm:update()
+    -- Code here gets called every frame
+
+    super.update(self)
+end
+
+return Tailofhellstorm
