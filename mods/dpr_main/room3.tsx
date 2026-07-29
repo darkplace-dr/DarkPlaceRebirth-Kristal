093ca967371b725b98c0fa743d7484a0c730d542
Nell <alex.cimer@mail.ru>
2026-07-29 18:20:15 +0300

    Imma slime you if that ever happens again

--- mods/dpr_main/room3.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.1" name="room3" tilewidth="40" tileheight="40" tilecount="24" columns="6">
- <image source="../../../../assets/sprites/tilesets/room3.png" width="240" height="180"/>
-</tileset>
