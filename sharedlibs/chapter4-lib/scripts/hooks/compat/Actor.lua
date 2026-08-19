a7148e82c880560b6ce49410d8b2b548402b715e
Diamond Deltahedron <183671108+DIAMONDDELTAHEDRON@users.noreply.github.com>
2026-08-18 18:04:12 -0700

    Fix shopkeeper actors crashing the game

--- sharedlibs/chapter4-lib/scripts/hooks/compat/Actor.lua
+++ sharedlibs/chapter4-lib/scripts/hooks/compat/Actor.lua
@@ -2,9 +2,10 @@
 local Actor, super = HookSystem.hookScript(Actor)
 
 function Actor:onSpriteInit(sprite)
-    if not self:getAnimation("jump_ball_slow") and self:getAnimation("jump_ball") then
+	--This breaks shopkeeper actors. Maybe find a better fix later?
+    --[[if not self:getAnimation("jump_ball_slow") and self:getAnimation("jump_ball") then
         self.animations["jump_ball_slow"] = self:getAnimation("jump_ball")
-    end
+    end]]
     super.onSpriteInit(self, sprite)
 end
 
