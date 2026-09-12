0b9ef8f34b861080d8184fd548eb14a349063c70
Nell <alex.cimer@mail.ru>
2026-09-12 19:49:23 +0300

    Titan Spawn tiny fix

--- mods/dpr_main/scripts/battle/enemies/hooks/titan_spawn.lua
+++ mods/dpr_main/scripts/battle/enemies/hooks/titan_spawn.lua
@@ -14,14 +14,13 @@ function TitanSpawn:onAct(battler, name)
         Game.battle:startActCutscene(function(cutscene)
             local kris = Game.battle:getPartyBattler("kris")
 			self.wake_kris_count = self.wake_kris_count + 1
-            cutscene:text("* "..battler.chara:getName().." used Raise Up!")
+            cutscene:text("* " .. battler.chara:getName() .. " used Raise Up!")
 			kris:shake()
 			cutscene:wait(0.5)
 			battler:setAnimation("battle/idle")
 			if kris then
-				local kris_member = Game:getPartyMember("kris")
-				if kris_member.health <= 0 then
-					local reviveamt = math.abs(kris_member.health) + 50
+				if kris.chara.health <= 0 then
+					local reviveamt = math.abs(kris.chara.health) + 50
 					kris:heal(reviveamt)
 				else
 					cutscene:text("* (But, Kris wasn't DOWNed...)")
@@ -31,6 +30,14 @@ function TitanSpawn:onAct(battler, name)
         return
     end
     return super.onAct(self, battler, name)
+end
+
+function TitanSpawn:onPurifyStart()
+	self.x = self.x + 300
+end
+
+function TitanSpawn:onPurifyEnd()
+	self:spare()
 end
 
 return TitanSpawn
